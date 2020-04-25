b017d5d99557984ea7aa4a70e64f432a78d8b4a2
Arseni Borisovs <aborisov@sfu.ca>
2020-04-25 13:55:54 -0700

    Move functions only used in index.js out of common.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ function selectElement(id, valueToSelect) {
 
 window.onload = function () {
     if (window.location.search.length !== 0) {
-        let info = this.parseQuery(window.location.search);
+        let info = common.parseQuery(window.location.search);
 
         selectElement("first_timeunit", info.first_timeunit);
         selectElement("first_timeval", info.first_timeval);
@@ -18,6 +18,45 @@ window.onload = function () {
 
 const form = document.querySelector("#timeSelector");
 form.onsubmit = function () {
-    common.askNotificationPermission();
+    askNotificationPermission();
     return true;
 };
+
+// From https://developer.mozilla.org/en-US/docs/Web/API/Notifications_API/Using_the_Notifications_API
+function askNotificationPermission() {
+    // function to actually ask the permissions
+    function handlePermission(permission) {
+        // Whatever the user answers, we make sure Chrome stores the information
+        if (!("permission" in Notification)) {
+            Notification.permission = permission;
+        }
+    }
+
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        console.log("This browser does not support notifications.");
+    }
+    else {
+        if (checkNotificationPromise()) {
+            Notification.requestPermission()
+                .then((permission) => {
+                    handlePermission(permission);
+                });
+        }
+        else {
+            Notification.requestPermission(function (permission) {
+                handlePermission(permission);
+            });
+        }
+    }
+}
+
+function checkNotificationPromise() {
+    try {
+        Notification.requestPermission().then();
+    } catch (e) {
+        return false;
+    }
+
+    return true;
+}
